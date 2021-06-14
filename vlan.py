a5b13387f8409715aaf5660f8b877c4ec56cb4b8
Jeong Arm <kjwonmail@gmail.com>
2021-06-14 15:03:03 +0900

    Set qdisc from config

--- vlan.py
+++ vlan.py
@@ -18,22 +18,67 @@ def create_vlan(ifname: str, vlanid: int) -> int:
         with open('config.yaml') as f:
             config = yaml.load(f, Loader=yaml.FullLoader)
 
+        ifconf = config['nics'][ifname]
+
         qos_map = ' '.join(
             f'{skb_pri}:{vlan_pri}'
             for skb_pri, vlan_pri
-            in config['nics'][ifname]['egress-qos-map'][vlanid].items())
+            in ifconf['egress-qos-map'][vlanid].items())
 
         subprocess.check_call(shlex.split(
-            # TODO: egress-qos-map
             f'ip link add link {ifname} name {name} type vlan id {vlanid} '
             f'egress-qos-map {qos_map}'
         ))
+        # Set mqprio
+        mqprio = ifconf['qdisc']['mqprio']
+        root_handle = mqprio['handle']
+        num_tc = mqprio['num_tc']
+        priomap = ' '.join(map(str, mqprio['map']))
+        queues = ' '.join(mqprio['queues'])
+        offload = mqprio.get('hw', False)
+        subprocess.check_call(shlex.split(
+            f'tc qdisc add dev {ifname} parent root '
+            f'handle {root_handle} mqprio '
+            f'num_tc {num_tc} '
+            f'map {priomap} '
+            f'queues {queues} '
+            f'hw {1 if offload else 0}'
+        ))
+
+        for qid, val in ifconf['qdisc']['child'].items():
+            t = val['type']
+            handle = val['handle']
+            if t == 'cbs':
+                idleslope = val['idleslope']
+                sendslope = val['sendslope']
+                hicredit = val['hicredit']
+                locredit = val['locredit']
+                subprocess.check_call(shlex.split(
+                    f'tc qdisc replace dev {ifname} '
+                    f'parent {root_handle}:{qid} '
+                    f'handle {handle} cbs '
+                    f'idleslope {idleslope} '
+                    f'sendslope {sendslope} '
+                    f'hicredit {hicredit} '
+                    f'locredit {locredit} '
+                    f'offload 1'
+                ))
+                subprocess.check_call(shlex.split(
+                    f'tc qdisc add dev {ifname} parent {handle}:1 etf '
+                    'clockid CLOCK_TAI '
+                    'delta 500000 '
+                    'offload '
+                ))
+            else:  # TODO: support taprio
+                raise ValueError(f'qdisc type {t} is not supported')
+
         subprocess.check_call(shlex.split(
             f'ip link set up {name}'
         ))
     except subprocess.CalledProcessError as e:
         return e.returncode
     except KeyError as e:
+        print(e)
         print('Config is not properly configuered', file=sys.stderr)
         return 1
     else:
@@ -45,6 +90,10 @@ def delete_vlan(ifname: str, vlanid: int) -> int:
     try:
         subprocess.check_call(shlex.split(
             f'ip link del {name}'
+        ))
+
+        subprocess.check_call(shlex.split(
+            f'tc qdisc delete dev {ifname} root'
         ))
     except subprocess.CalledProcessError as e:
         return e.returncode
