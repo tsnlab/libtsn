80f9dedc8e3607ab0b36b50c6b75da251aec7933
Jeong Arm <kjwonmail@gmail.com>
2021-07-12 12:39:35 +0900

    Add information CLI command

--- pytsn/__main__.py
+++ pytsn/__main__.py
@@ -4,6 +4,8 @@ import os
 import re
 import socket
 
+import yaml
+
 if True:
     import sys
 
@@ -16,37 +18,65 @@ if True:
 SOCKET_PATH = '/var/run/tsn.sock'
 
 
+def info(ifname: str = None):
+    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    sock.connect(SOCKET_PATH)
+    command = ('info' if not ifname else f'info {ifname}') + '\n'
+    sock.send(command.encode())
+    res = sock.makefile().read()
+    sock.close()
+
+    print(res)
+
+
+def get_info(config: dict, ifname: str = None):
+    if not ifname:
+        return config['nics']
+    else:
+        return {k: v for k, v in config['nics'].items() if k == ifname}
+
+
 def main():
-    parser = argparse.ArgumentParser(description='Daemon for TSN traffic control')
+    parser = argparse.ArgumentParser(
+        description='Daemon for TSN traffic control',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--config', '-c', default='config.yaml', help='Config file in yaml format')
     parser.add_argument('--bind', '-b', default=SOCKET_PATH, help='Unix socket path to bind')
 
-    sub_parser = parser.add_subparsers(help='Directly create or delete vlan', dest='command')
+    sub_parser = parser.add_subparsers(help='Directly interact with CLI', dest='command')
 
-    create_parser = sub_parser.add_parser('create')
-    create_parser.add_argument('interface', help='Interface to create vlan')
+    create_parser = sub_parser.add_parser('create', help='Create TSN interface from CLI')
+    create_parser.add_argument('interface', help='Interface to create')
     create_parser.add_argument('vlanid', type=int, help='Vlan id to create')
 
-    delete_parser = sub_parser.add_parser('delete')
-    delete_parser.add_argument('interface', help='Interface to delete vlan')
+    delete_parser = sub_parser.add_parser('delete', help='Delete TSN interface from CLI')
+    delete_parser.add_argument('interface', help='Interface to delete')
     delete_parser.add_argument('vlanid', type=int, help='Vlan id to delete')
 
-    arguments = parser.parse_args()
+    info_parser = sub_parser.add_parser('info', help='Get information about TSN interface')
+    info_parser.add_argument('interface', nargs='?', help='Interface to get info')
 
-    config = read_config(arguments.config)
+    info_parser = sub_parser.add_parser('daemon', help='Start TSN UCD daemon')
+
+    arguments = parser.parse_args()
 
     command_map = {
         'create': create_vlan,
         'delete': delete_vlan,
     }
 
-    if arguments.command:
-        return command_map[arguments.command](config, arguments.interface, arguments.vlanid)
+    if arguments.command in ('create', 'delete'):
+        config = read_config(arguments.config)
+        return command_map[arguments.command](config, arguments.ifname, arguments.vlanid)
+    elif arguments.command == 'info':
+        return info(arguments.interface)
 
+    config = read_config(arguments.config)
     server = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     server.bind(arguments.bind)
     server.listen(1)
-    pattern = re.compile(r'(?P<cmd>create|delete) (?P<ifname>\w+) (?P<vlanid>\d+)')
+    pattern_socket = re.compile(r'(?P<cmd>create|delete) (?P<ifname>\w+) (?P<vlanid>\d+)')
+    pattern_info = re.compile(r'info(?: (?P<ifname>\w+))?')
     with contextlib.ExitStack() as es:
         def cleanup():
             server.close()
@@ -58,15 +88,18 @@ def main():
             conn, addr = server.accept()
             line = conn.makefile().readline()
             print(f'{line=}')
-            matched = pattern.match(line)
-            if not matched:
-                conn.send(b'-1')
-            else:
+
+            if matched := pattern_socket.match(line):
                 cmd = matched.group('cmd')
                 ifname = matched.group('ifname')
                 vlanid = int(matched.group('vlanid'))
                 res = command_map[cmd](config, ifname, vlanid)
                 conn.send(f'{res}'.encode())
+            elif matched := pattern_info.match(line):
+                ifname = matched.group('ifname')
+                conn.send(yaml.dump(get_info(config, ifname)).encode())
+            else:
+                conn.send(b'-1')
             conn.close()
 
 
