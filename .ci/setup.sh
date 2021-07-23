fbc16948da65d042b3ca8363b783fb957e99afce
Jeong Arm <kjwonmail@gmail.com>
2021-07-23 13:09:08 +0900

    Don't require pgp

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -11,18 +11,3 @@ echo "$SSH_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
 ssh-keyscan "$REPO_HOST" >> ~/.ssh/known_hosts
-
-cat << EOF > ~/.dput.cf
-[DEFAULT]
-default_host_main = tsnlab
-
-[tsnlab]
-fqdn = $REPO_HOST
-method = scp
-login = $REPO_USER
-incoming = $REPO_PATH/incoming
-ssh_config_options =
-    StrictHostKeyChecking no
-EOF
-
-echo "$PGP_KEY" | gpg --import
