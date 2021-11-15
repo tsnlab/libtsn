d970d02a7bf433a465638cf36d2673f10bce8d1e
Jeong Arm <kjwonmail@gmail.com>
2021-11-15 15:09:37 +0900

    Add ifnames api endpoint

--- pytsn/webui/app.py
+++ pytsn/webui/app.py
@@ -1,3 +1,6 @@
+import glob
+import os
+
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
@@ -7,10 +10,17 @@ import yaml
 CONFIG_FILENAME = 'config.yaml'  # FIXME: use config
 
 app = FastAPI(title='libTSN Configuration')
-api = FastAPI()
+api = FastAPI(title="libTSN Web API")
 
 app.mount('/api', api)
 app.mount('/', StaticFiles(directory='build', html=True), name='front')
+
+
+@api.get('/ifnames')
+def read_ifnames():
+    virtual_ifnames = {p.split(os.path.sep)[-1] for p in glob.glob('/sys/devices/virtual/net/*')}
+    ifnames = {p.split(os.path.sep)[-1] for p in glob.glob('/sys/class/net/*')}
+    return ifnames - virtual_ifnames
 
 
 @api.get('/config/')
