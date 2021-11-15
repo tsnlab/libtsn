ed60880e58ad4aa5709b85338faa0fc684e180c0
Jeong Arm <kjwonmail@gmail.com>
2021-11-15 17:45:54 +0900

    WIP: Make config viewer

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import { Nic } from './Config';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_BASE_URL = (
+  (process.env.NODE_ENV === 'development') ?
+  'http://localhost:8000/api' :
+  '/api'
+);
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      config: {},
+      ifnames: [],
+      initialised: false,
+    };
+  }
+
+  async componentDidMount() {
+    const ifnames = fetch(`${API_BASE_URL}/ifnames`).then(res => res.json());
+    const config = fetch(`${API_BASE_URL}/config`).then(res => res.json());
+
+    console.debug(`Using ${API_BASE_URL}`);
+
+    Promise.all([ifnames, config]).then(([ifnames, config]) => {
+      this.setState({
+        ifnames,
+        config,
+        initialised: true,
+      });
+    });
+  }
+
+  render() {
+    const { config, ifnames } = this.state;
+
+    if (!this.state.initialised) {
+      return (<div>Loading...</div>);
+    }
+
+    return (
+      <div className="App">
+        <h1>Available NICs</h1>
+        {ifnames.map((ifname, index) => {
+          return (
+            <div key={ifname}>
+              <span>{index}: {ifname}</span>
+            </div>
+          );
+        })}
+
+        {Object.entries(config.nics).map(([key, value]) => {
+          return (
+            <Nic key={key} ifname={key} config={value} />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
