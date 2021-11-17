305b84f9f13186cbc7bfa72e928be2459935c032
Jeong Arm <kjwonmail@gmail.com>
2021-11-17 15:18:54 +0900

    Split Cbs into another file

--- dashboard/src/Cbs.js
+++ dashboard/src/Cbs.js
@@ -1,61 +1,4 @@
 import { Component } from 'react';
-import Tas from './Tas';
-
-class Nic extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      config: props.config,
-    };
-  }
-
-  updateTas = (value) => {
-    let config = {...this.state.config };
-    config.tas = value;
-    this.setState({
-      config,
-    });
-
-    this.props.update(config);
-  };
-
-  updateCbs = (value) => {
-    let config = {...this.state.config };
-    config.cbs = value;
-    this.setState({
-      config,
-    });
-
-    this.props.update(config);
-  };
-
-  render() {
-    const { ifname, config } = this.props;
-
-    let headers = [ <th>Option</th> ];
-
-    for (let i = -1; i < 8; i += 1) {
-      headers.push(<th>{i === -1 ? 'BE' : i}</th>);
-    }
-
-    return (
-      <div>
-        <h1>{ ifname }</h1>
-        <div className="schedulers">
-          <div>{ JSON.stringify(this.state.config) }</div>
-          <table>
-            <thead>
-              { headers }
-            </thead>
-            <Tas data={ config.tas || {} } update={ this.updateTas } />
-            <Cbs data={ config.cbs || {} } update={ this.updateCbs } />
-          </table>
-        </div>
-      </div>
-    );
-  }
-}
-
 
 class Cbs extends Component {
   constructor(props) {
@@ -120,4 +63,4 @@ class Cbs extends Component {
   }
 }
 
-export { Nic };
+export default Cbs;
