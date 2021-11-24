67658e7b17bf552b4c5d0cf92c5c680c05c32040
Jeong Arm <kjwonmail@gmail.com>
2021-11-24 15:52:48 +0900

    Implement tcp on latency

--- examples/latency/latency.c
+++ examples/latency/latency.c
@@ -40,11 +40,11 @@ static struct argp_option options[] = {
     {"verbose", 'v', 0, 0, "Produce verbose output"},
     {"interface", 'i', "IFACE", 0, "Interface name to use"},
     {"server", 's', 0, 0, "Server mode"},
-    {"client", 'c', 0, 0, "Client mode"},
-    {"target", 't', "TARGET", 0, "Target MAC addr"},
+    {"client", 'c', "TARGET", 0, "Client mode"},
     {"count", 'C', "COUNT", 0, "How many send packet"},
     {"size", 'l', "BYTES", 0, "Packet size in bytes"},
-    {"ipv4", '4', 0, 0, "Use IPv4"},
+    {"tcp", 't', 0, 0, "Use TCP/IPv4"},
+    {"udp", 'u', 0, 0, "Use UDP/IPv4"},
     {"precise", 'X', 0, 0, "Send packet at precise 0ns"},
     {"oneway", '1', 0, 0, "Check latency on receiver side"},
     {0},
@@ -57,7 +57,8 @@ enum run_mode {
 
 enum packet_type {
     PACKET_RAW = 1,
-    PACKET_IPV4,
+    PACKET_TCP,
+    PACKET_UDP,
 };
 
 struct arguments {
@@ -87,9 +88,13 @@ static error_t parse_opt(int key, char* arg, struct argp_state* state) {
         break;
     case 'c':
         arguments->mode = RUN_CLIENT;
+        arguments->target = arg;
         break;
     case 't':
-        arguments->target = arg;
+        arguments->pkt_type = PACKET_TCP;
+        break;
+    case 'u':
+        arguments->pkt_type = PACKET_UDP;
         break;
     case 'C':
         arguments->count = atoi(arg);
@@ -102,9 +107,6 @@ static error_t parse_opt(int key, char* arg, struct argp_state* state) {
         break;
     case '1':
         arguments->oneway = true;
-        break;
-    case '4':
-        arguments->pkt_type = PACKET_IPV4;
         break;
     case ARGP_KEY_ARG:
         argp_usage(state);
@@ -121,8 +123,11 @@ static struct argp argp = {options, parse_opt, args_doc, doc};
 void do_server(int sock, int size, bool oneway, bool verbose);
 void do_client(int sock, char* iface, int size, char* target, int count, bool precise, bool oneway);
 
-void do_server_l3(int sock, int size, bool oneway, bool verbose);
-void do_client_l3(int sock, char* iface, int size, char* target, int count, bool precise, bool oneway);
+void do_server_udp(int sock, int size, bool oneway, bool verbose);
+void do_client_udp(int sock, char* iface, int size, char* target, int count, bool precise, bool oneway);
+
+void do_server_tcp(int sock, int size, bool oneway, bool verbose);
+void do_client_tcp(int sock, char* iface, int size, char* target, int count, bool precise, bool oneway);
 
 bool strtomac(uint8_t* mac, const char* str);
 bool mactostr(uint8_t* mac, char* str);
@@ -174,8 +179,11 @@ int main(int argc, char** argv) {
         sock = tsn_sock_open(arguments.iface, VLAN_ID_PERF, VLAN_PRI_PERF, ETHERTYPE_PERF);
         break;
 
-    case PACKET_IPV4:
+    case PACKET_UDP:
         sock = tsn_sock_open_l3(arguments.iface, VLAN_ID_PERF, VLAN_PRI_PERF, AF_INET, SOCK_DGRAM, 0);
+        break;
+    case PACKET_TCP:
+        sock = tsn_sock_open_l3(arguments.iface, VLAN_ID_PERF, VLAN_PRI_PERF, AF_INET, SOCK_STREAM, 0);
         break;
     }
 
@@ -189,16 +197,21 @@ int main(int argc, char** argv) {
     if (arguments.mode == RUN_SERVER) {
         if (arguments.pkt_type == PACKET_RAW) {
             do_server(sock, arguments.size, arguments.oneway, arguments.verbose);
+        } else if (arguments.pkt_type == PACKET_UDP) {
+            do_server_udp(sock, arguments.size, arguments.oneway, arguments.verbose);
         } else {
-            do_server_l3(sock, arguments.size, arguments.oneway, arguments.verbose);
+            do_server_tcp(sock, arguments.size, arguments.oneway, arguments.verbose);
         }
     } else if (arguments.mode == RUN_CLIENT) {
         if (arguments.pkt_type == PACKET_RAW) {
             do_client(sock, arguments.iface, arguments.size, arguments.target, arguments.count, arguments.precise,
                       arguments.oneway);
+        } else if (arguments.pkt_type == PACKET_UDP) {
+            do_client_udp(sock, arguments.iface, arguments.size, arguments.target, arguments.count, arguments.precise,
+                          arguments.oneway);
         } else {
-            do_client_l3(sock, arguments.iface, arguments.size, arguments.target, arguments.count, arguments.precise,
-                         arguments.oneway);
+            do_client_tcp(sock, arguments.iface, arguments.size, arguments.target, arguments.count, arguments.precise,
+                          arguments.oneway);
         }
     } else {
         fprintf(stderr, "Unknown mode\n");
@@ -376,7 +389,7 @@ void do_client(int sock, char* iface, int size, char* target, int count, bool pr
     }
 }
 
-void do_server_l3(int sock, int size, bool oneway, bool verbose) {
+void do_server_udp(int sock, int size, bool oneway, bool verbose) {
     uint8_t* pkt = malloc(size);
     if (pkt == NULL) {
         fprintf(stderr, "Failed to malloc pkt\n");
@@ -395,19 +408,9 @@ void do_server_l3(int sock, int size, bool oneway, bool verbose) {
         exit(1);
     }
 
-    // This is for TCP
-    /*
-    if (listen(sock, 1) < 0) {
-        fprintf(stderr, "Failed to listen\n");
-        exit(1);
-    }
-    */
-
     struct pkt_perf* payload = (struct pkt_perf*)(pkt + sizeof(struct ethhdr));
 
     struct timespec tstart, tend, tdiff;
-
-    char srcmac[18], dstmac[18];
 
     const size_t controlsize = 1024;
     char control[controlsize];
@@ -460,11 +463,9 @@ void do_server_l3(int sock, int size, bool oneway, bool verbose) {
             fflush(stdout);
         }
     }
-
-    close(sock);
 }
 
-void do_client_l3(int sock, char* iface, int size, char* target, int count, bool precise, bool oneway) {
+void do_client_udp(int sock, char* iface, int size, char* target, int count, bool precise, bool oneway) {
     uint8_t* pkt = malloc(size);
     if (pkt == NULL) {
         fprintf(stderr, "Failed to malloc pkt\n");
@@ -523,6 +524,196 @@ void do_client_l3(int sock, char* iface, int size, char* target, int count, bool
             bool received = false;
             do {
                 int len = recvfrom(sock, pkt, size, 0, (struct sockaddr*)&serv_addr, &serv_addr_size);
+                clock_gettime(CLOCK_REALTIME, &tend);
+
+                tsn_timespec_diff(&tstart, &tend, &tdiff);
+                // Check perf pkt
+                if (len < 0 && tdiff.tv_nsec >= TIMEOUT_SEC) {
+                    // TIMEOUT
+                    break;
+                } else if (ntohl(payload->id) == i) {
+                    received = true;
+                }
+            } while (!received && running);
+
+            if (received) {
+                uint64_t elapsed_ns = tdiff.tv_sec * 1000000000 + tdiff.tv_nsec;
+                printf("RTT: %lu.%03lu µs (%lu → %lu)\n", elapsed_ns / 1000, elapsed_ns % 1000, tstart.tv_nsec,
+                       tend.tv_nsec);
+                fflush(stdout);
+            } else {
+                printf("TIMEOUT: -1 µs (%lu → N/A)\n", tstart.tv_nsec);
+                fflush(stdout);
+            }
+        }
+
+        if (!precise) {
+            request.tv_sec = 0;
+            request.tv_nsec = 700 * 1000 * 1000 + (random() % 10000000);
+            nanosleep(&request, NULL);
+        }
+    }
+}
+
+void do_server_tcp(int sock, int size, bool oneway, bool verbose) {
+    uint8_t* pkt = malloc(size);
+    if (pkt == NULL) {
+        fprintf(stderr, "Failed to malloc pkt\n");
+        exit(1);
+    }
+
+    struct sockaddr_in addr;
+
+    bzero(&addr, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(PORT_PERF);
+
+    if (bind(sock, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+        fprintf(stderr, "Failed to bind\n");
+        exit(1);
+    }
+
+    if (listen(sock, 1) < 0) {
+        fprintf(stderr, "Failed to listen\n");
+        exit(1);
+    }
+
+    struct pkt_perf* payload = (struct pkt_perf*)(pkt + sizeof(struct ethhdr));
+
+    struct timespec tstart, tend, tdiff;
+
+    const size_t controlsize = 1024;
+    char control[controlsize];
+
+    struct msghdr msg;
+    struct iovec iov = {pkt, size};
+    msg.msg_iov = &iov;
+    msg.msg_iovlen = 1;
+    msg.msg_control = &control;
+    msg.msg_controllen = controlsize;
+    struct cmsghdr* cmsg;
+
+    int sockflags;
+    sockflags = SOF_TIMESTAMPING_RX_HARDWARE | SOF_TIMESTAMPING_RAW_HARDWARE | SOF_TIMESTAMPING_SOFTWARE;
+    int res = setsockopt(sock, SOL_SOCKET, SO_TIMESTAMPNS, &sockflags, sizeof(sockflags));
+    if (res < 0) {
+        perror("Socket timestampns");
+    }
+
+    while (running) {
+        struct sockaddr_in cli_addr;
+        socklen_t cli_addr_size;
+        cli_addr_size = sizeof(cli_addr);
+
+        int cli_sock = accept(sock, (struct sockaddr*)&cli_addr, &cli_addr_size);
+
+        if (cli_sock < 0) {
+            perror("Failed to accept");
+            continue;
+        }
+
+        do {
+            size_t recv_bytes = recv(cli_sock, pkt, size, 0);
+            if (recv_bytes < 0) {
+                break;
+            }
+
+            if (oneway) {
+                clock_gettime(CLOCK_REALTIME, &tend);
+                for (cmsg = CMSG_FIRSTHDR(&msg); cmsg != NULL; cmsg = CMSG_NXTHDR(&msg, cmsg)) {
+                    int level = cmsg->cmsg_level;
+                    int type = cmsg->cmsg_type;
+                    if (level != SOL_SOCKET)
+                        continue;
+                    if (SO_TIMESTAMPNS == type) {
+                        memcpy(&tend, CMSG_DATA(cmsg), sizeof(tend));
+                    }
+                }
+            }
+
+            send(cli_sock, pkt, recv_bytes, 0);
+
+            if (oneway) {
+                char src_ip[INET_ADDRSTRLEN], dst_ip[INET_ADDRSTRLEN];
+                inet_ntop(AF_INET, &cli_addr.sin_addr, src_ip, INET_ADDRSTRLEN);
+                tstart.tv_sec = ntohl(payload->tv_sec);
+                tstart.tv_nsec = ntohl(payload->tv_nsec);
+                tsn_timespec_diff(&tstart, &tend, &tdiff);
+                printf("%08x %s %s %lu.%09lu → %lu.%09lu %ld.%09lu\n", ntohl(payload->id), src_ip, dst_ip,
+                       tstart.tv_sec, tstart.tv_nsec, tend.tv_sec, tend.tv_nsec, tdiff.tv_sec, tdiff.tv_nsec);
+                fflush(stdout);
+            }
+        } while (running);
+
+        close(cli_sock);
+    }
+}
+
+void do_client_tcp(int sock, char* iface, int size, char* target, int count, bool precise, bool oneway) {
+    uint8_t* pkt = malloc(size);
+    if (pkt == NULL) {
+        fprintf(stderr, "Failed to malloc pkt\n");
+        exit(1);
+    }
+
+    struct timeval timeout = {TIMEOUT_SEC, 0};
+    if (setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) < 0) {
+        perror("Set socket timeout");
+        return;
+    }
+
+    struct pkt_perf* payload = (struct pkt_perf*)(pkt + sizeof(struct ethhdr));
+
+    struct timespec tstart, tend, tdiff;
+    struct timespec request, error_gettime, error_nanosleep;
+
+    if (precise) {
+        tsn_time_analyze();
+    }
+
+    fprintf(stderr, "Starting\n");
+
+    struct sockaddr_in serv_addr;
+    socklen_t serv_addr_size;
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(target);
+    serv_addr.sin_port = htons(PORT_PERF);
+
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Failed to connect");
+        return;
+    }
+
+    for (int i = 0; i < count && running; i += 1) {
+
+        payload->id = htonl(i);
+
+        if (precise) {
+            // Sleep to x.000000000s
+            clock_gettime(CLOCK_REALTIME, &request);
+            request.tv_sec += 1;
+            request.tv_nsec = 0;
+            tsn_time_sleep_until(&request);
+        }
+
+        clock_gettime(CLOCK_REALTIME, &tstart);
+
+        payload->tv_sec = htonl(tstart.tv_sec);
+        payload->tv_nsec = htonl(tstart.tv_nsec);
+
+        int sent = send(sock, pkt, size, 0);
+        if (sent < 0) {
+            perror("Failed to send");
+        }
+
+        serv_addr_size = sizeof(serv_addr);
+
+        if (!oneway) {
+            bool received = false;
+            do {
+                int len = recv(sock, pkt, size, 0);
                 clock_gettime(CLOCK_REALTIME, &tend);
 
                 tsn_timespec_diff(&tstart, &tend, &tdiff);
