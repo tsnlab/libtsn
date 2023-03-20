c05ddb587570fc1e4112d6cba812e763b88d5a79
goforlee <goforlee@gmail.com>
2023-03-20 10:52:55 +0900

    fix perfdata bug

--- tsn/src/bin/throughput.rs
+++ tsn/src/bin/throughput.rs
@@ -442,7 +442,6 @@ fn do_client(sock: &mut i32, iface: String, size: i32, target: String, time: i32
     pkt.clear();
     pkt_info_bytes.clear();
 
-    pkt = ethernet_bytes.clone();
     pkt_info.id = 1;
     pkt_info.op = PerfOpcode::PerfData as u8;
     tstart = clock_gettime(ClockId::CLOCK_MONOTONIC).unwrap();
@@ -452,6 +451,7 @@ fn do_client(sock: &mut i32, iface: String, size: i32, target: String, time: i32
     while RUNNING.load(Ordering::Relaxed) && tdiff.tv_sec() < time as i64 {
         pkt.clear();
         pkt = ethernet_bytes.clone();
+        pkt_info_bytes.clear();
         pkt_info_bytes = bincode::serialize(&pkt_info).unwrap();
         pkt.append(&mut pkt_info_bytes);
         // println!("id = {}", pkt_info.id);
@@ -463,6 +463,7 @@ fn do_client(sock: &mut i32, iface: String, size: i32, target: String, time: i32
         tend = clock_gettime(ClockId::CLOCK_MONOTONIC).unwrap();
         tsn::tsn_timespecff_diff(&mut tstart, &mut tend, &mut tdiff);
     }
+    println!("pkt array =  {:0x?}", pkt);
 
     let last_sent_id = pkt_info.id;
     println!("last sent id = {}", last_sent_id);
