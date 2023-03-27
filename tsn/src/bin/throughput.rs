0ff36ba52de63ef44d54f420742a62505469c16a
goforlee <goforlee@gmail.com>
2023-03-27 11:30:13 +0900

    add print inside of match expression

--- tsn/src/bin/throughput.rs
+++ tsn/src/bin/throughput.rs
@@ -139,6 +139,8 @@ fn do_server(sock: &mut i32, size: i32) {
                 let pkt_info_bytes = bincode::serialize(&pkt_info).unwrap();
                 ethernet.set_payload(&pkt_info_bytes);
 
+                println!("ethernet packet = {:0x?} ", ethernet.packet_mut());
+
                 send_perf(sock, ethernet.packet_mut(), recv_bytes as usize);
             }
             PerfOpcode::ReqEnd => {
@@ -159,7 +161,6 @@ fn do_server(sock: &mut i32, size: i32) {
                 pkt_info.op = PerfOpcode::ResEnd as u8;
                 let pkt_info_bytes: Vec<u8> = bincode::serialize(&pkt_info).unwrap();
                 ethernet.set_payload(&pkt_info_bytes);
-                println!("ethernet packet = {:0x?} ", ethernet.packet_mut());
                 send_perf(sock, ethernet.packet_mut(), recv_bytes as usize);
             }
             PerfOpcode::ReqResult => {
