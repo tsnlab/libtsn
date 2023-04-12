9d3e54db433bcfc7f5ddf248cbe6ff51cf784b18
Jeong Arm <kjwonmail@gmail.com>
2023-04-12 15:39:23 +0900

    Change libtsn signatures, Add impl for TsnSocket

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use nix::net::if_::if_nametoindex;
 use nix::sys::socket::msghdr;
 use nix::sys::time::{TimeSpec, TimeValLike};
@@ -12,7 +14,26 @@ extern crate socket;
 pub struct TsnSocket {
     pub fd: i32,
     pub ifname: String,
-    pub vlanid: u32,
+    pub vlanid: u16,
+}
+
+// Make imple for TsnSocket
+impl TsnSocket {
+    pub fn set_timeout(&mut self, timeout: Duration) -> i32 {
+        sock_set_timeout(self, timeout)
+    }
+
+    pub fn send(&self, buf: &[u8]) -> isize {
+        send(self, buf)
+    }
+
+    pub fn recv(&self, buf: &mut [u8]) -> isize {
+        recv(self, buf)
+    }
+
+    pub fn recv_msg(&self, msg: msghdr) -> isize {
+        recv_msg(self, msg)
+    }
 }
 
 const CONTROL_SOCK_PATH: &str = "/var/run/tsn.sock";
@@ -25,21 +46,21 @@ fn send_cmd(command: String) -> Result<String, std::io::Error> {
     Ok(msg)
 }
 
-fn create_vlan(ifname: &str, vlanid: u32) -> Result<String, std::io::Error> {
+fn create_vlan(ifname: &str, vlanid: u16) -> Result<String, std::io::Error> {
     let command = format!("create {} {}\n", ifname, vlanid);
     send_cmd(command)
 }
 
-fn delete_vlan(ifname: &str, vlanid: u32) -> Result<String, std::io::Error> {
+fn delete_vlan(ifname: &str, vlanid: u16) -> Result<String, std::io::Error> {
     let command = format!("delete {} {}\n", ifname, vlanid);
     send_cmd(command)
 }
 
-pub fn tsn_sock_open(
+pub fn sock_open(
     ifname: &str,
-    vlanid: u32,
+    vlanid: u16,
     priority: u32,
-    proto: u32,
+    proto: u16,
 ) -> Result<TsnSocket, i32> {
     match create_vlan(ifname, vlanid) {
         Ok(v) => println!("{}", v),
@@ -57,7 +78,7 @@ pub fn tsn_sock_open(
         sock = libc::socket(
             libc::AF_PACKET,
             libc::SOCK_RAW,
-            socket::htons(proto as u16) as libc::c_int,
+            socket::htons(proto) as libc::c_int,
         );
     }
     if sock < 0 {
@@ -110,8 +131,8 @@ pub fn tsn_sock_open(
     })
 }
 
-pub fn tsn_sock_close(sock: &mut TsnSocket) {
-    match delete_vlan(&(*sock).ifname, sock.vlanid) {
+pub fn sock_close(sock: &mut TsnSocket) {
+    match delete_vlan(&sock.ifname, sock.vlanid) {
         Ok(v) => {
             println!("{}", v);
             close(sock.fd).unwrap();
@@ -123,12 +144,29 @@ pub fn tsn_sock_close(sock: &mut TsnSocket) {
     };
 }
 
-pub fn tsn_send(sock: i32, buf: *mut u8, n: i32) -> isize {
+pub fn sock_set_timeout(sock: &mut TsnSocket, timeout: Duration) -> i32 {
+    let sock_timeout = libc::timeval {
+        tv_sec: timeout.as_secs() as i64,
+        tv_usec: timeout.subsec_micros() as i64,
+    };
+
+    unsafe {
+        libc::setsockopt(
+            sock.fd,
+            libc::SOL_SOCKET,
+            libc::SO_RCVTIMEO,
+            &sock_timeout as *const libc::timeval as *const libc::c_void,
+            mem::size_of::<libc::timeval>() as u32,
+        )
+    }
+}
+
+pub fn send(sock: &TsnSocket, buf: &[u8]) -> isize {
     unsafe {
         libc::sendto(
-            sock,
-            buf as *mut libc::c_void,
-            n as usize,
+            sock.fd,
+            buf.as_ptr() as *const libc::c_void,
+            buf.len(),
             0,
             std::ptr::null_mut::<libc::sockaddr>(),
             0_u32,
@@ -136,28 +174,28 @@ pub fn tsn_send(sock: i32, buf: *mut u8, n: i32) -> isize {
     }
 }
 
-pub fn tsn_recv(sock: i32, buf: *mut u8, n: i32) -> isize {
+pub fn recv(sock: &TsnSocket, buf: &mut [u8]) -> isize {
     unsafe {
         libc::recvfrom(
-            sock,
-            buf as *mut libc::c_void,
-            n as usize,
-            0_i32, /* flags */
+            sock.fd,
+            buf.as_ptr() as *mut libc::c_void,
+            buf.len(),
+            0 as libc::c_int, /* flags */
             std::ptr::null_mut::<libc::sockaddr>(),
             std::ptr::null_mut::<u32>(),
         )
     }
 }
 
-pub fn tsn_recv_msg(sock: i32, mut msg: msghdr) -> isize {
-    unsafe { libc::recvmsg(sock, &mut msg as *mut msghdr, 0) }
+pub fn recv_msg(sock: &TsnSocket, mut msg: msghdr) -> isize {
+    unsafe { libc::recvmsg(sock.fd, &mut msg as *mut msghdr, 0) }
 }
 
-pub fn tsn_timespecff_diff(start: &mut TimeSpec, stop: &mut TimeSpec, result: &mut TimeSpec) {
+pub fn timespecff_diff(start: &mut TimeSpec, stop: &mut TimeSpec, result: &mut TimeSpec) {
     if start.tv_sec() > stop.tv_sec()
         || (start.tv_sec() == stop.tv_sec() && start.tv_nsec() > stop.tv_nsec())
     {
-        tsn_timespecff_diff(start, stop, result);
+        timespecff_diff(start, stop, result);
         let result_sec: TimeSpec = TimeValLike::seconds(result.tv_sec());
         let result_nsec: TimeSpec = TimeValLike::nanoseconds(result.tv_nsec());
         *result = (result_sec * -1) + result_nsec;
