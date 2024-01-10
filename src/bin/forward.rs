452733af2140814813f09cc1e8f1feb67ee8dc48
pak-ji <wlgnsehfdl@naver.com>
2024-01-10 16:42:54 +0900

    Add forward

--- src/bin/forward.rs
+++ src/bin/forward.rs
@@ -1,28 +1,13 @@
-use std::io::Error;
-use std::mem;
-use std::option::Option;
 use std::thread;
-use std::time::{Duration, Instant, SystemTime, UNIX_EPOCH};
-use std::vec::Vec;
-
-use nix::sys::socket::{cmsghdr, msghdr};
-use num_derive::FromPrimitive;
-use num_traits::FromPrimitive;
-use rand::Rng;
+use clap::{Command, arg};
 use signal_hook::{consts::SIGINT, iterator::Signals};
 
-use clap::{arg, crate_authors, crate_version, Command};
+static mut RUNNING: bool = false;
 
-use pnet_macros::packet;
-use pnet_macros_support::types::u32be;
-use pnet_packet::{MutablePacket, Packet};
-
-use pnet::datalink::{self, NetworkInterface};
-use pnet::packet::ethernet::{EtherType, EthernetPacket, MutableEthernetPacket};
-use pnet::util::MacAddr;
-use tsn::time::tsn_time_sleep_until;
-
-extern crate socket as soc;
+#[allow(non_upper_case_globals)]
+static mut iface1_sock: tsn::TsnSocket = tsn::TsnSocket { fd: 0, ifname: String::new(), vlanid: 0 };
+#[allow(non_upper_case_globals)]
+static mut iface2_sock: tsn::TsnSocket = tsn::TsnSocket { fd: 0, ifname: String::new(), vlanid: 0 };
 
 fn main() {
     let matches = Command::new("forward")
@@ -31,19 +16,74 @@ fn main() {
         .get_matches();
 
     let nic1_name = matches.get_one::<String>("nic1").unwrap().clone();
-    let nic2_name = matches.get_one::<String>("nic1").unwrap().clone();
+    let nic2_name = matches.get_one::<String>("nic2").unwrap().clone();
 
-    do_l2_forward(&nic1_name, &nic2_name);
+    do_l2_forward(nic1_name, nic2_name);
 }
 
-fn do_l2_forward(nic1: &String, nic2: &String) {
-    /*
-     * TODO)
-     *  1. Create TSN Sockets for nic1 & nic2 without vlan_off
-     *  2. Receive L2 Packets from nic1 & nic2 TSN Sockets
-     *  3. Send L2 Packets to nic1 & nic2 TSN Sockets
-     *
-     *  2 & 3. There is no processing in between.
-     *  It simply sends the L2 packet to another NIC socket.
-     */
+fn do_l2_forward(nic1_name: String, nic2_name: String) {
+    let vlan_off = true;
+    let proto = libc::ETH_P_ALL as u16;
+
+    /* FIXME) unsafe... ToT... */
+    unsafe {
+        /* Init iface1 socket */
+        iface1_sock = match tsn::sock_open(&nic1_name, 0, 0, proto, vlan_off)
+        {
+            Ok(sock) => sock,
+            Err(e) => panic!("Failed to open TSN socket: {}", e)
+        };
+        println!("iface1 socket init successful");
+
+        /* Init iface2 socket */
+        iface2_sock = match tsn::sock_open(&nic2_name, 0, 0, proto, vlan_off)
+        {
+            Ok(sock) => sock,
+            Err(e) => panic!("Failed to open TSN socket: {}", e)
+        };
+        println!("iface2 socket init successful");
+
+        /* Add to interrupt handler */
+        RUNNING = true;
+        let mut signals = Signals::new([SIGINT]).unwrap();
+        thread::spawn(move || {
+            for _ in signals.forever() {
+                    RUNNING = false;
+            }
+        });
+
+        /* Do forward : NIC1 -> NIC2 */
+        let handle_1 = thread::spawn(move || {
+            let mut packet = vec![0; 1500];
+            while RUNNING {
+                let _ = match iface1_sock.recv(&mut packet) {
+                    Ok(n) => iface2_sock.send(&packet[0..n as usize]),
+                    Err(_) => continue,
+                };
+            }
+        });
+
+        /* Do forward : NIC2 -> NIC1 */
+        let handle_2 = thread::spawn(move || {
+            let mut packet = vec![0; 1500];
+            while RUNNING {
+                let _ = match iface2_sock.recv(&mut packet) {
+                    Ok(n) => iface1_sock.send(&packet[0..n as usize]),
+                    Err(_) => continue,
+                };
+            }
+        });
+
+        handle_1.join().unwrap();
+        handle_2.join().unwrap();
+
+        /* Close to TSN Sockets */
+        if iface1_sock.close().is_err() {
+            eprintln!("Failed to close socket");
+        }
+        if iface2_sock.close().is_err() {
+            eprintln!("Failed to close socket");
+        }
+
+    } /* unsafe */
 }
