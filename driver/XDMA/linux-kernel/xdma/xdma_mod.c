22339c29524ef0b0616376ea089db10862c9264d
Junho Lee <junho@tsnlab.com>
2024-12-23 16:38:04 +0900

    Fix multiqueue handling issues (#64)
    * Add subqueue operations
    
    * Add xdma_select_queue()
    
    * Fix compile error for older versions of gcc
    
    * Add comment for select_queue()

--- driver/XDMA/linux-kernel/xdma/xdma_mod.c
+++ driver/XDMA/linux-kernel/xdma/xdma_mod.c
@@ -226,6 +226,7 @@ static const struct net_device_ops xdma_netdev_ops = {
 	.ndo_start_xmit = xdma_netdev_start_xmit,
 	.ndo_setup_tc = xdma_netdev_setup_tc,
 	.ndo_eth_ioctl = xdma_netdev_ioctl,
+	.ndo_select_queue = xdma_select_queue,
 };
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 11, 0)
--- driver/XDMA/linux-kernel/xdma/xdma_netdev.c
+++ driver/XDMA/linux-kernel/xdma/xdma_netdev.c
@@ -65,9 +65,13 @@ int xdma_netdev_open(struct net_device *ndev)
         struct xdma_private *priv = netdev_priv(ndev);
         u32 lo, hi;
         unsigned long flag;
+        int i;
 
         netif_carrier_on(ndev);
         netif_start_queue(ndev);
+        for (i = 0; i < TX_QUEUE_COUNT; i++) {
+                netif_start_subqueue(ndev, i);
+        }
 
         /* Set the RX descriptor */
         priv->rx_desc->src_addr_lo = cpu_to_le32(PCI_DMA_L(priv->res_dma_addr));
@@ -91,9 +95,13 @@ int xdma_netdev_open(struct net_device *ndev)
 
 int xdma_netdev_close(struct net_device *ndev)
 {
+        int i;
         struct xdma_private *priv = netdev_priv(ndev);
         iowrite32(DMA_ENGINE_STOP, &priv->rx_engine->regs->control);
         netif_stop_queue(ndev);
+        for (i = 0; i < TX_QUEUE_COUNT; i++) {
+                netif_stop_subqueue(ndev, i);
+        }
         pr_info("xdma_netdev_close\n");
         netif_carrier_off(ndev);
         pr_info("netif_carrier_off\n");
@@ -113,9 +121,12 @@ netdev_tx_t xdma_netdev_start_xmit(struct sk_buff *skb,
         struct tx_buffer* tx_buffer;
         struct tx_metadata* tx_metadata;
         u32 to_value;
+        u16 q;
+
 
         /* Check desc count */
-        netif_stop_queue(ndev);
+        q = skb_get_queue_mapping(skb);
+        netif_stop_subqueue(ndev, q);
         xdma_debug("xdma_netdev_start_xmit(skb->len : %d)\n", skb->len);
         skb->len = max((unsigned int)ETH_ZLEN, skb->len);
 
@@ -125,7 +136,7 @@ netdev_tx_t xdma_netdev_start_xmit(struct sk_buff *skb,
 
         if (skb_padto(skb, skb->len)) {
                 pr_err("skb_padto failed\n");
-                netif_wake_queue(ndev);
+                netif_wake_subqueue(ndev, q);
                 dev_kfree_skb(skb);
                 return NETDEV_TX_OK;
         }
@@ -133,7 +144,7 @@ netdev_tx_t xdma_netdev_start_xmit(struct sk_buff *skb,
         /* Jumbo frames not supported */
         if (skb->len > XDMA_BUFFER_SIZE) {
                 pr_err("Jumbo frames not supported\n");
-                netif_wake_queue(ndev);
+                netif_wake_subqueue(ndev, q);
                 dev_kfree_skb(skb);
                 return NETDEV_TX_OK;
         }
@@ -143,7 +154,7 @@ netdev_tx_t xdma_netdev_start_xmit(struct sk_buff *skb,
 #ifdef __LIBXDMA_DEBUG__
                 pr_err("pskb_expand_head failed\n");
 #endif
-                netif_wake_queue(ndev);
+                netif_wake_subqueue(ndev, q);
                 dev_kfree_skb(skb);
                 return NETDEV_TX_OK;
         }
@@ -168,7 +179,7 @@ netdev_tx_t xdma_netdev_start_xmit(struct sk_buff *skb,
 #ifdef __LIBXDMA_DEBUG__
                 pr_warn("tsn_fill_metadata failed\n");
 #endif
-                netif_wake_queue(ndev);
+                netif_wake_subqueue(ndev, q);
                 return NETDEV_TX_BUSY;
         }
 
@@ -180,7 +191,7 @@ netdev_tx_t xdma_netdev_start_xmit(struct sk_buff *skb,
         dma_addr = dma_map_single(&xdev->pdev->dev, skb->data, skb->len, DMA_TO_DEVICE);
         if (unlikely(dma_mapping_error(&xdev->pdev->dev, dma_addr))) {
                 pr_err("dma_map_single failed\n");
-                netif_wake_queue(ndev);
+                netif_wake_subqueue(ndev, q);
                 return NETDEV_TX_BUSY;
         }
 
@@ -230,6 +241,11 @@ netdev_tx_t xdma_netdev_start_xmit(struct sk_buff *skb,
 
         iowrite32(DMA_ENGINE_START, &priv->tx_engine->regs->control);
         return NETDEV_TX_OK;
+}
+
+u16 xdma_select_queue(struct net_device *ndev, struct sk_buff *skb, struct net_device *sb_dev) {
+        /* Always use the first subqueue */
+        return 0;
 }
 
 static LIST_HEAD(xdma_block_cb_list);
